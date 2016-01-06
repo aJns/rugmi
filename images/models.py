00ec22c6fccab004054b33e93f24c84d4fee6db3
Jonas Nikula <jonas.nikula@student.tut.fi>
2016-01-06 19:58:20 +0000

    Test models

--- images/models.py
+++ images/models.py
@@ -4,11 +4,14 @@ from django.db import models
 class Submission(models.Model):
     title = models.CharField(max_length=75)
     description = models.CharField(max_length=500)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(auto_now_add=True)
     content = models.ImageField()
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
     submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
     comment_text = models.CharField(max_length=500)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(auto_now_add=True)
--- rugmi/settings.py
+++ rugmi/settings.py
@@ -126,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Media files
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = './test_images/'        # Development directory, change for production!
